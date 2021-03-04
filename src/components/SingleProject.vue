f5b59ae76651491ab5b283f6b69313e41d9d0720
root <root@mol.uio.no>
2021-03-04 12:44:07 +0100

    Edited the data file for json server. index.html file got the icon CDN. Added a new component for the projects i.e. singleProject, project.id is passed & gotten via props.

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="project">
+      <div class="actions" >
+          <h3 @click="showDetailAction()"> {{project.title}} </h3>
+
+        <div class="details" v-if="showDetails">
+           <p> {{project.details}} </p>
+        </div>
+            <div class="icons">
+                <span class="material-icons">done_all</span> 
+                <span class="material-icons">edit</span> 
+                <span @click="deleteProject" class="material-icons">delete</span> 
+            </div>
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['project'],
+    data() {
+        return {
+             showDetails: false,
+             uri: 'http://mol.uio.no:3000/projects/'+this.project.id
+        }
+    },
+    methods: {
+        showDetailAction(){
+            this.showDetails=!this.showDetails;
+            console.log("Showing Details");
+        },
+        deleteProject() {
+            fetch(this.uri, {method: 'DELETE'});
+            console.log("Deleting Project");
+
+        }
+    }
+
+}
+</script>
+
+<style>
+.project {
+    margin: 2rem auto;
+    background: rgb(216, 213, 213);
+    padding: 1rem 2rem;
+    border-radius: .4rem;
+    box-shadow: .8rem .5rem 1rem rgba(0, 0, 0, .5);
+    border-left: .4rem solid rgba(77, 75, 73, 0.116);
+}
+h3 {
+    cursor: pointer;
+}
+.actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.material-icons {
+    font-size: 1.4rem;
+    margin-left: 1rem;
+    color: rgb(148, 154, 155);
+    cursor: pointer;
+}
+.material-icons:hover {
+    color: rgb(121, 126, 126);
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,24 +2,25 @@
   <div class="home">
       <div v-if="projects.length">
           <div v-for="project in projects" :key="project.id">
-            <p> {{ project.title }} </p>
+            <SingleProject :project="project"/>
           </div>
       </div>
   </div>
 </template>
 
 <script>
+import  SingleProject from '../components/SingleProject.vue'
 
 export default {
   name: 'Home',
-  components: {},
+  components: {SingleProject},
     data() {
       return {
         projects: []
       }
     },  
   mounted() {
-    fetch("http://mol.uio.no:3000/project")
+    fetch("http://mol.uio.no:3000/projects")
     .then(res => res.json())
     .then(data => this.projects = data)
     .then(adeel => {console.log("ADEEL TEST LOG",adeel);})
