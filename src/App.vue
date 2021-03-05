b8fd0904669c0028131d53cc637dd1f5aae68144
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2021-03-05 19:17:39 +0100

    Header has been added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <Navigation/>
+  <Header />
+  <Navigation class="slim" />
   <router-view/>
 </template>
 
 <script>
 import Navigation from './components/Navigation.vue'
+import Header from './components/Header.vue'
 export default {
-  components: { Navigation }
+  components: { Navigation, Header }
   
 
 }
 </script>
 <style>
 body {
-  background: white;
+  background: linear-gradient(to right bottom, rgba(122, 124, 128, 0.658), rgba(104, 187, 235, 0.603));
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  max-width: 600px;
+  max-width: auto;
   margin: 0 auto;
-  color: #565;
+  color: rgb(85, 100, 102);
 }
-
-
+.slim {
+  max-width: 90%;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header"> 
+    <div class="logo-box">
+       <img src="../../public/img/logo2.png" alt="Logo" class="logo">
+    </div>
+    <div class="logo-box2">
+       <img src="../../public/img/logo2.png" alt="Logo" class="logo">
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style >
+.header {
+  height: 30vh;
+  background-image: linear-gradient(to right bottom, rgba(153, 181, 233, 0.459), rgba(104, 187, 235, 0.404)),
+   url(../../public/img/banner.jpg);
+  background-size: cover;
+  background-size: cover;
+  background-position: top;
+  position: relative;
+  clip-path: polygon(0% 0%, 100% 0, 100% 25vh, 0% 100%); 
+}
+.logo-box{
+    position: absolute;
+    top: .9rem;
+    left: .1rem; 
+  }
+.logo-box2 {
+    position: absolute;
+    top: .9rem;
+    right: .1rem; 
+  }
+.logo {
+    height: 8.5rem; 
+    }
+</style>
