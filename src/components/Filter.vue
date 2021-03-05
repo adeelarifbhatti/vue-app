784baa5a59a56409ecf78b80d32654c7e1910566
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2021-03-05 16:33:01 +0100

    Added the filter for projects

---
+++ src/components/Filter.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="filter">
+      <button class="btn-filter" @click="updateFilter('all')"
+      :class="{active: current ==='all'}">
+
+          View All
+      </button>
+        <button class="btn-filter" @click="updateFilter('completed')"
+              :class="{active: current ==='completed'}">
+          Completed
+      </button>
+        <button class="btn-filter" @click="updateFilter('incompleted')"
+              :class="{active: current ==='incompleted'}">
+          Incompleted
+      </button>
+  </nav>
+</template>
+
+<script>
+export default {
+    props: ['current'],
+    methods: {
+        updateFilter(by) {
+            this.$emit('filterChange',by);
+        }
+    }
+}
+</script>
+
+<style>
+
+.btn-filter{ 
+    text-transform: uppercase;
+    text-decoration: none;
+    margin-bottom: .7rem;
+    margin-top: .1rem;
+    margin-right: .5rem;
+    padding: .1rem 1rem;
+    display: inline-block;
+    border-radius: .5rem;
+    transition: all .1s;
+    background-color: rgba(209, 226, 208, 0.411);
+    color: rgb(24, 22, 19);
+    }
+.filter button.active {
+    color: rgb(149, 226, 123);
+        border-radius: 0rem;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
+    <Filter @filterChange="current = $event" :current="current"/>
       <div v-if="projects.length">
-          <div v-for="project in projects" :key="project.id">
+          <div v-for="project in filteredProjects" :key="project.id">
             <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"/>
           </div>
       </div>
@@ -10,13 +11,16 @@
 
 <script>
 import  SingleProject from '../components/SingleProject.vue'
+import  Filter from '../components/Filter.vue'
+
 
 export default {
   name: 'Home',
-  components: {SingleProject},
+  components: {SingleProject,Filter}, 
     data() {
       return {
-        projects: []
+        projects: [],
+        current: 'all'
       }
     },  
   mounted() {
@@ -38,6 +42,20 @@ export default {
       });
       p.complete = !p.complete
       }
+  },
+  computed: {
+    filteredProjects() {
+      if (this.current === 'completed') {
+        return this.projects.filter(project => project.complete)
+      }
+      if (this.current ==='incompleted'){
+        return this.projects.filter(project => !project.complete)
+      }
+      return this.projects
+    }
   }  
 }
 </script>
+<style>
+
+</style>
